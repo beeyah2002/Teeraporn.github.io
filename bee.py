381a6b36b3fd61719c614dd747797dfc1dad58e9
beeyah2002 <beesui2545@gmail.com>
2022-10-24 23:10:01 +0700

    updatenowww

--- bee.py
+++ bee.py
@@ -1,11 +1,32 @@
-from  turtle import *
-color('red')
-begin_fill()
-pensize(3)
-left(50)
-forward(133)
-circle(50,200)
-right(140)
-circle(50,200)
-forward(133)
-end_fill
+#the circle module has functions that perform
+#calculations related to circles.
+import math
+
+#the area function accepts a circle's reduis as an
+#argument and return the area of the circle.
+def area(radius):
+    return math.pi * radius**2
+
+    #The circumference function accepts a circle's radius
+    #ans returns the area of the circle.
+def area(radius):
+    return math.pi * radius
+
+
+
+
+
+
+#the rectangle module has function that perform
+#calculations related o rectangles.
+
+#the srae function accept a rectangle's width and
+#lenght as argument and returns the rectangle's area
+def area(width,lenght):
+    return width * lenght
+
+#the perimeter function accepts a rectangle's width
+#and length as arguments and returns the rectangle's perimeter.
+
+def perimeter(width, length):
+    return 2 * (width + length)
